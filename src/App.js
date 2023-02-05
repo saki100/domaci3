8010a1df2656961485af1b3b098ba0ef801cd943
saki100 <sara.spanovic10@gmail.com>
2023-02-05 16:39:57 +0100

    Kreirana komponenta Facts.jsx

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Facts from './komponente/Facts';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+  return(
+    <div>
+      <Router>
+      <NavBar/>
+        <Routes>
+          <Route path="/" element={<Facts/>} />
+        </Routes>
+      </Router >
     </div>
   );
 }
