c03650b84c230f60c5dc5e960028d0fe2e540c4d
saki100 <sara.spanovic10@gmail.com>
2023-02-05 16:53:10 +0100

    Prevlacenje podataka sa api-ja

--- src/App.js
+++ src/App.js
@@ -2,15 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Facts from './komponente/Facts';
+import { useState } from 'react';
+import FactInfo from './komponente/FactInfo';
 
 function App() {
+
+  const [facts, setFacts] = useState([]);
+  const[oneFact, setOneFact]=useState(facts[0])
+
+    useEffect(() => {
+        axios.get('https://catfact.ninja/facts?limit=50').then(res => {
+            console.log(JSON.stringify(res))
+            setFacts(res.data.data)
+        });
+    }, []);
   
   return(
     <div>
       <Router>
       <NavBar/>
         <Routes>
-          <Route path="/" element={<Facts/>} />
+          <Route path="/" element={<Facts  facts={facts} setOneFact={setOneFact}/>} />
+          <Route path="/info" element={<FactInfo oneFact={oneFact}/>}/>
         </Routes>
       </Router >
     </div>
---
+++ src/komponente/FactInfo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FactInfo() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default FactInfo
