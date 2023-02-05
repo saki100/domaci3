803a998dd30f48331b089f9f21a8613a003e529a
saki100 <sara.spanovic10@gmail.com>
2023-02-05 17:33:50 +0100

    Kartice za svaki pojedinacni citat

--- src/komponente/FactCard.jsx
+++ src/komponente/FactCard.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 function FactCard({fact}) {
   return (
-    <div>
-      <h3>{fact.fact}</h3>
+    <div class="card">
+        <div class="card-header">
+            <img src="https://static.vecteezy.com/system/resources/previews/009/665/304/original/cute-kitty-cat-head-cartoon-element-free-png.png" className="card-img-top" alt="cat"></img>
+            <i>#Fact</i>
+        </div>
+        <div class="card-body">
+            <p class="card-text"><b>{fact.fact}</b></p>
+            <button className="btn btn-primary btn-sm">Detaljnije</button>
+        </div>
     </div>
   )
 }
